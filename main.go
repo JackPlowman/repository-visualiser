4464075e5e7bfed0410822c2ee458d04ece36455
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-01-28 16:12:22 +0000

    update

--- main.go
+++ main.go
@@ -8,10 +8,27 @@ import (
 	"github.com/go-enry/go-enry/v2"
 )
 
+// main is the entry point of the application.
 func main() {
-	var languageCountArray LanguageCountArray
-	languageCountArray = recursiveFileSearch(languageCountArray)
+	languageCountArray := recursiveFileSearch(".")
 	fmt.Println(languageCountArray)
+	writeSummary(languageCountArray)
+}
+
+// writeSummary writes the language count array to the GitHub Action summary if available.
+func writeSummary(languageCountArray LanguageCountArray) {
+	actionSummaryPath := os.Getenv("GITHUB_STEP_SUMMARY")
+	if actionSummaryPath != "" {
+		file, err := os.OpenFile(actionSummaryPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		defer file.Close()
+		for _, lc := range languageCountArray {
+			fmt.Fprintf(file, "%s: %d\n", lc.Language, lc.Count)
+		}
+	}
 }
 
 type LanguageCount struct {
@@ -21,37 +38,36 @@ type LanguageCount struct {
 
 type LanguageCountArray []LanguageCount
 
-func recursiveFileSearch(languageCountArray LanguageCountArray) LanguageCountArray {
-	root := "."
+// recursiveFileSearch recursively searches files in the provided root directory and returns language counts.
+func recursiveFileSearch(root string) LanguageCountArray {
+	languageCountMap := make(map[string]int)
 
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		if !info.IsDir() {
-			languageCountArray = detectLanguage(languageCountArray, path)
+			content, err := os.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			language := enry.GetLanguage(path, content)
+			languageCountMap[language]++
 		}
 		return nil
 	})
-
 	if err != nil {
-		fmt.Printf("Error walking the path %q: %v\n", root, err)
+		fmt.Println("Error:", err)
 	}
 
-	return languageCountArray
+	return mapToLanguageCountArray(languageCountMap)
 }
 
-func detectLanguage(languageCountArray LanguageCountArray, filePath string) LanguageCountArray {
-	language, _ := enry.GetLanguageByExtension(filePath)
-	fmt.Println(language)
-	if language == "" {
-		language = "Unknown"
+// mapToLanguageCountArray converts a map of language counts to a LanguageCountArray.
+func mapToLanguageCountArray(languageCountMap map[string]int) LanguageCountArray {
+	languageCountArray := make(LanguageCountArray, 0, len(languageCountMap))
+	for language, count := range languageCountMap {
+		languageCountArray = append(languageCountArray, LanguageCount{Language: language, Count: count})
 	}
-	for i := range languageCountArray {
-		if languageCountArray[i].Language == language {
-			languageCountArray[i].Count++
-			return languageCountArray
-		}
-	}
-	return append(languageCountArray, LanguageCount{Language: language, Count: 1})
+	return languageCountArray
 }
